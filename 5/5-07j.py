c0baa56dc48e7f48e1d1003861c3519e986c21c6
JunFujino <j_fujino@fm.me.es.osaka-u.ac.jp>
2020-05-27 12:19:46 +0900

    modified:   5-07j.py

--- 5/5-07j.py
+++ 5/5-07j.py
@@ -22,9 +22,9 @@ for i in range(0,itr+1):
         v = v - k * x * dt / m -  B * v * dt / m + E * dt * np.sin(w*t) / m
         x = x + v * dt
         t = t + dt
-        if x>7.5:
+        if abs(x)>7.5:   #7.5を超えたら，共振して極大になると判断した．グラフができなければ，ここの値を小さくすればいい
             flg=1
-    if flg==1:
+    if flg==1:  #共振したときのグラフのみ表示する
         w = dw * float(i)
         t = 0.0
         v = 0.0
