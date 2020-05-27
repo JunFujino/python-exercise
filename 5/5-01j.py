7a29beae8446db16f9e2a30cdc55e90ecbfa8b06
JunFujino <j_fujino@fm.me.es.osaka-u.ac.jp>
2020-05-27 13:14:29 +0900

    modified:   5-01j.py 	modified:   5-03j.py

--- 5/5-01j.py
+++ 5/5-01j.py
@@ -5,5 +5,5 @@ itr = 10
 plt.scatter(n,a,s=5,c="blue")
 for n in range(1,itr+1):
     a = 2 * a + 1
-    plt.scatter(n,a,s=5,c="blue")
-plt.show()
+    plt.scatter(n,a,s=5,c="blue")   # 点を表示
+plt.show()  #表示
--- 5/5-03j.py
+++ 5/5-03j.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
-v = 0.0     #初期条件
+#初期条件
+v = 0.0
 t = 0.0
+#パラメータ
 g = 9.80
 dt = 0.10
 nstep = 400
+#点を表示していく
 plt.scatter(v,t,s=5,c="blue")
 for n in range(1,nstep+1):
     v = v + g * dt
