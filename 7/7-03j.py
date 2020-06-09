34a1a3711dc64c2aeb83e86c535f0a6603f1d6e4
JunFujino <j_fujino@fm.me.es.osaka-u.ac.jp>
2020-06-09 12:46:49 +0900

    modified:   7/7-02j.py 	modified:   7/7-03j.py

--- 7/7-03j.py
+++ 7/7-03j.py
@@ -1,2 +1,15 @@
 import numpy as np
 import matplotlib.pyplot  as plt
+import random
+N = 1000
+x = np.zeros(N+1)
+x[0] = 0.0
+dx = 1.0
+for i in range(0,N):
+    a = random.random()
+    if a>0.50:
+        x[i+1] = x[i] + dx
+    else:
+        x[i+1] = x[i] - dx
+plt.plot(x,'-o')
+plt.show()
