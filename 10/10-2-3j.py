d7d463cedf3e593439f68571353cb20340a25cc9
JunFujino <j_fujino@fm.me.es.osaka-u.ac.jp>
2020-06-30 13:35:25 +0900

    modified:   10/10-2-3j.py

---
+++ 10/10-2-3j.py
@@ -0,0 +1,82 @@
+import matplotlib.animation as animation
+from numpy import sin, cos
+import numpy as np
+import matplotlib.pyplot as plt
+G = 9.8   # acceleration due to gravity, in m/s^2
+L1 = 1.0  # length of pendulum 1 in m
+L2 = 1.0  # length of pendulum 2 in m
+M1 = 1.0  # mass of pendulum 1 in kg
+M2 = 1.0  # mass of pendulum 2 in kg
+
+def derivs(x):
+    F = np.zeros_like(x)
+    F[0] = x[1]
+    delta = x[2] - x[0]
+    den1 = (M1+M2) * L1 - M2 * L1 * cos(delta) * cos(delta)
+    F[1] = ((M2 * L1 * x[1] * x[1] * sin(delta) * cos(delta)
+                + M2 * G * sin(x[2]) * cos(delta)
+                + M2 * L2 * x[3] * x[3] * sin(delta)
+                - (M1+M2) * G * sin(x[0]))
+               / den1)
+    F[2] = x[3]
+    den2 = (L2/L1) * den1
+    F[3] = ((- M2 * L2 * x[3] * x[3] * sin(delta) * cos(delta)
+                + (M1+M2) * G * sin(x[0]) * cos(delta)
+                - (M1+M2) * L1 * x[1] * x[1] * sin(delta)
+                - (M1+M2) * G * sin(x[2]))
+               / den2)
+    return F
+
+dt = 0.0001
+N=100000
+# th1 and th2 are the initial angles (degrees)
+# w10 and w20 are the initial angular velocities (degrees per second)
+th1 = 120.0
+w1 = 0.0
+th2 = -10.0 #- 0.010 (case1)
+w2 = 0.0
+# initial conditions
+x = np.radians([th1, w1, th2, w2])
+xh=np.zeros((N,4))
+for i in range(N):
+    x = x + derivs(x)*dt
+    xh[i,:]=x[:]
+t = np.arange(0, int(dt*N), dt)
+#plt.plot(t,xh[:,0],c="red")
+#plt.plot(t,xh[:,2],c="blue")
+#plt.xlabel('t')
+#plt.show()
+j = 0  
+M = 100
+xh = np.zeros ((int(N/M),4))
+
+for i in range(N):
+    x = x + derivs(x)*dt
+    if i % M == 0:      #Mステップおきに出力
+        xh[j,:]=x[:]
+        j = j + 1
+#==========================================以下アニメーション用==================================================
+x1 = L1*sin(xh[:, 0])
+y1 = -L1*cos(xh[:, 0])
+x2 = L2*sin(xh[:, 2]) + x1
+y2 = -L2*cos(xh[:, 2]) + y1
+fig = plt.figure ()
+ax = fig.add_subplot (111,  autoscale_on=False , xlim=(-2, 2), ylim=(-2, 2))
+ax.set_aspect('equal')
+ax.grid()
+line , = ax.plot([], [],'o-', lw = 2)
+time_template ='time␣=␣%.1fs'
+time_text = ax.text (0.05, 0.9,'', transform=ax.transAxes)
+def init ():
+    line.set_data ([], [])
+    time_text.set_text('')
+    return line , time_text
+def animate(i):
+    thisx = [0, x1[i], x2[i]]
+    thisy = [0, y1[i], y2[i]]
+    line.set_data(thisx , thisy)
+    time_text.set_text(time_template % (i*dt*M))
+    return line , time_text
+ani = animation.FuncAnimation(fig , animate ,int(N/M),interval=dt*N, blit=True , init_func=init)
+ani.save('double_pendulum.gif',writer='pillow',fps=50)
+plt.show()
