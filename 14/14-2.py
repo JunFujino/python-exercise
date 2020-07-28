3ccfa7ca02f30d48b1ce8cf0f8768839b2bb4acc
JunFujino <j_fujino@fm.me.es.osaka-u.ac.jp>
2020-07-28 19:16:32 +0900

    new file:   14-2.py

---
+++ 14/14-2.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+fig = plt.figure(figsize=(5,5))     #アニメーション用
+ims = []                            #アニメーション用
+#====================================
+#   パラメータ
+#====================================
+Nt = 2000
+L = 30
+V = 1.0
+A = 1.0
+#====================================
+#   パラメータと初期条件
+#====================================
+dt = 0.1
+x = 0.0
+v = 0.0
+
+for n in range(Nt+1):
+    v = v + A * (V - v) * dt
+    x = x + v * dt
+
+    if(x>=L):
+        x = x - L
+    theta = 2*np.pi*x/L
+    X = np.cos(theta)
+    Y = np.sin(theta)
+    
+    im = plt.plot(X,Y,'ob')     #アニメーション用
+    ims.append(im)              #アニメーション用
+
+ani = animation.ArtistAnimation(fig, ims, interval = 10)        #アニメーション用
+plt.show()
